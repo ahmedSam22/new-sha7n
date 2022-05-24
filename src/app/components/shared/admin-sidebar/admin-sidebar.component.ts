144574cddcc3109443d79da9d233eb0732505432
Shrouk Ahmed <shrouq.ahmed93@gmail.com>
2022-05-24 18:47:10 +0200

    last update shrouq and eng ahmed

--- src/app/components/shared/admin-sidebar/admin-sidebar.component.ts
+++ src/app/components/shared/admin-sidebar/admin-sidebar.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSidebarComponent implements OnInit {
   showFiller = false;
+  collapsed=true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  openNav() {
+    //  document.getElementById('mySidebar').style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+  }
+  closeNav() {
+    // document.getElementById("mySidebar").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+  }
 }
