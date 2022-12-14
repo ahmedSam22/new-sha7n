77a6bae0c723aa760eaae32479dc7c8601e84c8b
ahmedSam22 <tahmsamir288@tutamail.com>
2022-12-14 14:27:08 +0200

    update test

--- src/app/components/admin/orders/orders.component.ts
+++ src/app/components/admin/orders/orders.component.ts
@@ -61,15 +61,11 @@ export class OrdersAdminComponent implements OnInit {
      this.statusOrder(this.type)
     console.log("resssss orderrrrsss",  this.orders );
     var links = document.getElementsByTagName("a");
-    console.log("llllllllllllllllllllllinkssssssssss",links)
     for (var i = 0; i < links.length; i++) {
       this.firstSelect=links[11].id
-      console.log("llllllllllllllllllllllinkssssssssss",this.firstSelect)
       this.isActive(this.firstSelect);
       this.select(this.firstSelect) 
     }
-    
- 
 }
 isActive(item:any) {
   return  this.selected === item;
